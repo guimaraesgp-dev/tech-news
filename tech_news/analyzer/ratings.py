dfc074121fef69d359c3bbc131089cff261b47c1
Gabriel Guimarães <gabrielguimaraes.dev@gmail.com>
2023-10-23 14:23:56 -0300

    REQ 10

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,4 +1,15 @@
+from tech_news.database import get_collection
+
+
 # Requisito 10
 def top_5_categories():
-    """Seu código deve vir aqui"""
-    raise NotImplementedError
+    aggregation_pipeline = [
+        {"$group": {"_id": "$category", "count": {"$sum": 1}}},
+        {"$sort": {"count": -1, "_id": 1}},
+        {"$limit": 5},
+    ]
+
+    return [
+        result["_id"]
+        for result in get_collection().aggregate(aggregation_pipeline)
+    ]
